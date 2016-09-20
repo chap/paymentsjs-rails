61fea2a4acb038151a8b735d4b8d8b28e7775df4
Joshua Bartlett <joshua.r.bartlett@gmail.com>
2016-09-19 22:01:25 -0500

    Core functions

---
+++ lib/paymentsjs-rails.rb
@@ -0,0 +1,73 @@
+class PaymentsJs
+	require 'helpers/configuration'
+	require 'openssl'
+	require 'base64'
+	require 'json'
+	
+	extend Configuration
+	
+	def self.generate_req_id
+		req_id = "invoice" + rand(10...42).to_s
+		req_id
+	end
+	
+	def self.iv
+		iv = OpenSSL::Random.pseudo_bytes(16)
+		iv
+	end
+
+	define_setting :mid, "999999999997"
+	define_setting :mkey, "K3QD6YWyhfD"
+	define_setting :api_key, "7SMmEF02WyC7H5TSdG1KssOQlwOOCagb"
+	define_setting :api_secret, "wtC5Ns0jbtiNA8sP"
+	define_setting :req_id, PaymentsJs.generate_req_id
+	define_setting :request_type, "payment"
+	define_setting :postback_url, "https://www.example.com"
+	define_setting :amount, "1.00"
+	define_setting :pre_auth, false
+	define_setting :environment, "cert"
+	define_setting :secret, "wtC5Ns0jbtiNA8sP"
+	define_setting :iv, PaymentsJs.iv
+	
+	def self.salt
+		salt = PaymentsJs.iv.unpack('H*').first
+		salt = salt.bytes.to_a
+		salt = salt.pack('U*')
+		salt = Base64.strict_encode64(salt)
+		salt
+	end
+	
+	def self.req
+		mid          = PaymentsJs.mid
+		mkey         = PaymentsJs.mkey
+		api_key      = PaymentsJs.api_key
+		api_secret   = PaymentsJs.api_secret
+		req_id       = PaymentsJs.req_id
+		request_type = PaymentsJs.request_type
+		postback_url = PaymentsJs.postback_url
+		amount       = PaymentsJs.amount
+		pre_auth     = PaymentsJs.pre_auth
+		environment  = PaymentsJs.environment
+		secret       = PaymentsJs.secret
+		
+		req = {mid: mid, mkey: mkey, api_key: api_key, api_secret: api_secret, req_id: req_id, request_type: request_type, postback_url: postback_url, amount: amount, pre_auth: pre_auth, environment: environment, secret: secret }
+		
+		req
+	end
+	
+	def self.encrypt
+		cipher     = OpenSSL::Cipher::AES.new(256, :CBC)
+		cipher.encrypt
+		req        = PaymentsJs.req
+		secret     = PaymentsJs.secret
+		data       = JSON.generate(req)
+		salt       = PaymentsJs.salt
+		key        = OpenSSL::PKCS5.pbkdf2_hmac_sha1(secret, salt, 1500, 32)
+		cipher.key = key
+		cipher.iv  = PaymentsJs.iv
+		authKey    = cipher.update(data) + cipher.final()
+		authKey    = Base64.strict_encode64(authKey)
+		authKey
+	end
+	
+end
---
+++ paymentsjs-rails.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+s.name        = 'paymentsjs-rails'
+s.version     = '0.0.0'
+s.date        = '2016-09-19'
+s.summary     = "Sage PaymentsJS Gem"
+s.description = "A gem to simplify the encryption of data for the Sage PaymentsJS SDK for usage in Rails apps."
+s.authors     = ["Joshua Bartlett"]
+s.email       = "joshua.r.bartlett@gmail.com"
+s.files       = Dir["{lib,vendor}/**/*"] + "README.md"
+s.homepage    = "http://rubygems.org/gems/paymentjs-rails"
+s.license     = 'MIT'
+end
